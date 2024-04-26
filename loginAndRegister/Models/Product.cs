d8289d6c4849b80039e9f52372cc8ef65c6d342c
Ryan Habis <ryan.habiss@gmail.com>
2024-04-26 14:54:00 +0100

    Adding the product class for cart functionalityAdded aditional security to users logged in

---
+++ loginAndRegister/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+
+namespace starSystemV2.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
