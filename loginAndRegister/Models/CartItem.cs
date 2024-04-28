4d17b3d97a3c5c6534749c47600ef2360e4a3dc6
cgarvey00 <conorgarvey19@outlook.com>
2024-04-28 15:23:29 +0100

    Adding the foundations for the shopping cart which will be implemented into the website, pending more features for better user experience

---
+++ loginAndRegister/Models/CartItem.cs
@@ -0,0 +1,14 @@
+﻿namespace starSystemV2.Models
+{
+    public class CartItem
+    {    public int CartItemId {get; set; }
+
+        public int ProductId { get; set;}
+        public Product Product { get; set; }
+
+        public int Quantity { get; set; }
+
+public int ShoppingCartId { get; set; }
+        public ShoppingCart ShoppingCart { get; set; }
+    }
+}
---
+++ loginAndRegister/Models/ShoppingCart.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace starSystemV2.Models
+{
+    public class ShoppingCart
+    {
+        [Key]
+        public int ShoppingCartId { get; set; }
+
+        public string UserId { get; set; }
+
+        // Dictionary to store items, which is keyed by the productid
+        public Dictionary<int, CartItem> Items { get; set; } = new Dictionary<int, CartItem>();
+
+        public void AddItem(Product product,int quantity)
+        {
+            if(Items.TryGetValue(product.ProductId,out CartItem existingItem))
+            {
+                existingItem.Quantity += quantity;
+            }
+            else
+            {
+                Items[product.ProductId] = new CartItem { Product = product, Quantity = quantity };
+            }
+        }
+        public void RemoveItem(int productId)
+        {
+            if (Items.ContainsKey(productId))
+            {
+                Items.Remove(productId);
+            }
+        }
+
+    }
+}
