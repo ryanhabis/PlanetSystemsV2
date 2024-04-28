ee8b8a8130b8381a7b6e66bcf487af9e2d93e41d
cgarvey00 <conorgarvey19@outlook.com>
2024-04-28 23:15:10 +0100

    Initalising the Nasa APOD(Astronomy Picture Of the Day) class and the service in order to implement it in the landing page

---
+++ loginAndRegister/Data/NasaApodAPIService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using starSystemV2.Models;
+
+namespace starSystemV2.Data
+{
+    public class NasaApodAPIService
+    {
+        public readonly HttpClient _HttpClient;
+        private readonly string _apiKey = "Ax5bhqGAapzLl7HPIXuUfQlCSNUQHe7FNoUa5uhR";
+
+        public NasaApodAPIService(HttpClient httpClient)
+        {
+            _HttpClient = httpClient;
+        }
+
+        public async Task<NasaApod> GetApodAsync()
+        {
+            string url = $"https://api.nasa.gov/planetary/apod?api_key={_apiKey}";
+            var response = await _HttpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                return Newtonsoft.Json.JsonConvert.DeserializeObject<NasaApod>(content);
+            }
+            return null;
+
+        }
+    }
---
+++ loginAndRegister/Models/NasaApod.cs
@@ -0,0 +1,10 @@
+﻿namespace starSystemV2.Models
+{//Authors: Conor and Ryan
+    public class NasaApod
+    {
+        public string Date { get;set; }
+        public string Explanation { get; set; }
+        public string Url { get; set; }
+        public string Title { get; set; }
+    }
+}
